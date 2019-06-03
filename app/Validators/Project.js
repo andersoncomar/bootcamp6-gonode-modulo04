de6cb6a3dbb25b11ca3182834e504dd578bbed2a
Anderson Comar <andersoncomar@MacBook-Air-de-Anderson.local>
2019-06-02 23:14:01 -0300

    Modulo 4 concluido

---
+++ app/Validators/Project.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const Antl = use('Antl')
+
+class Project {
+  get rules () {
+    return {
+      title: 'required',
+      description: 'required'
+    }
+  }
+
+  get messages () {
+    return Antl.list('validation')
+  }
+}
+
+module.exports = Project
